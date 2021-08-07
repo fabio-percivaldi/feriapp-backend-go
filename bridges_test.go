d6abb6769281ec1d9d9289d2ddb678cfaf8b35e5
Fabio Percivaldi <fabio.percivaldi@mia-platform.eu>
2021-08-07 14:15:10 +0200

    multiple years are passed if scope is gt 1

--- bridges_test.go
+++ bridges_test.go
@@ -54,4 +54,47 @@ func TestBridgesRoutes(testCase *testing.T) {
 		require.NoError(t, readBodyError)
 		require.Equal(t, string(expectedResponse), string(body), "The response body should be the expected one")
 	})
+
+	testCase.Run("/bridges - 2 years scope", func(t *testing.T) {
+		bridgesArray := []bridges.Bridge{}
+		currentYear := time.Now().UTC()
+		YearBridges := []bridges.YearBridges{{
+			Years:         []string{strconv.FormatInt(int64(currentYear.Year()), 10)},
+			Bridges:       bridgesArray,
+			HolidaysCount: 6,
+			WeekdaysCount: 4,
+			DaysCount:     10,
+		},
+			{
+				Years:         []string{strconv.FormatInt(int64(currentYear.AddDate(1, 0, 0).Year()), 10)},
+				Bridges:       bridgesArray,
+				HolidaysCount: 6,
+				WeekdaysCount: 4,
+				DaysCount:     10,
+			}}
+
+		expectedResponse, _ := json.Marshal(YearBridges)
+
+		responseRecorder := httptest.NewRecorder()
+
+		requestBody, _ := json.Marshal(bridges.BridgesRequest{
+			DayOfHolidays:  2,
+			CustomHolidays: []bridges.CustomHolidays{},
+			City:           "Milan",
+			DaysOff:        []int{0, 6},
+			YearsScope:     2,
+		})
+
+		request, requestError := http.NewRequest(http.MethodPost, "/bridges", bytes.NewBuffer(requestBody))
+		require.NoError(t, requestError, "Error creating the /bridges request")
+
+		testRouter.ServeHTTP(responseRecorder, request)
+		statusCode := responseRecorder.Result().StatusCode
+		require.Equal(t, http.StatusOK, statusCode, "The response statusCode should be 200")
+
+		rawBody := responseRecorder.Result().Body
+		body, readBodyError := ioutil.ReadAll(rawBody)
+		require.NoError(t, readBodyError)
+		require.Equal(t, string(expectedResponse), string(body), "The response body should be the expected one")
+	})
 }
